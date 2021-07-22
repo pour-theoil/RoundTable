12bdedd9042095cb9dedd389eaccbd19d2a4e563
Logan Demmy <frank.demmy@gmail.com>
2021-07-22 14:52:15 -0500

    view all sources

---
+++ RoundTable/Repositories/ISourceRepository.cs
@@ -0,0 +1,19 @@
+﻿using RoundTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoundTable.Repositories
+{
+    public interface ISourceRepository
+    {
+        List<Source> GetAllSouces(int reporterId);
+        Source GetSouceById(int souceId);
+        void AddSource(Source source);
+        void UpdateSource(Source source);
+        void DeleteSource(int id);
+
+
+    }
+}
